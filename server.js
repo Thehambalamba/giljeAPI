3bf6836fc1fea5846de646f35ea24206d974132d
Nikola Pervic <nikolapervic@Nikolas-MacBook-Pro.local>
2017-05-19 00:24:31 +0200

    server-konfiguracija
    Kreiranje server.js fajla koji poziva naše zavisnosti i kreira express
    app. Konfiguracija express aplikacije za CORS zahteve, konekcija sa
    bazom, koršćenje morgana za logovanje. Zahtevanje ruta iz api fajla
    koji ćemo kreirati da rukuje našim rutama. Pokretanje na
    config.portu(8080).

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var jwt = require('jsonwebtoken');
+var config = require('./config');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET', 'POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
+    next();
+});
+
+// logovanje morganom
+app.use(morgan('dev'));
+
+// db konekcija
+mongoose.connect(config.database);
+
+var apiRoutes = require('./app/routes/api')(app, express);
+app.use('/api', apiRoutes);
+
+
+app.listen(config.port);
+console.log('Magic happens on port ' + config.port);
